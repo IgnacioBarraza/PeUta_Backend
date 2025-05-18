215872ad75bec6f4672d16f1a48ecfcff2cbf122
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-05-18 13:04:41 -0400

    :sparkles: feat: add linter and prettier

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+
+module.exports = tseslint.config({
+  files: ['**/*.ts'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+  ],
+  rules: {
+    '@typescript-eslint/no-require-imports': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+})
