4dc0a6ea6b9d938977f9a1c1260cc137d4227726
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-05-19 23:54:30 -0400

    ✨ feat: enhance sendResponse function to support setting cookies

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,28 @@
-import { Request, Response } from 'express'
+import { CookieOptions, Request, Response } from 'express'
+
+interface CookieData {
+  name: string
+  value: string
+  options?: CookieOptions
+}
 
 export function sendResponse(
   request: Request,
   response: Response,
   message: unknown,
-  status = 200
+  status = 200,
+  cookies?: CookieData[]
 ) {
+  if (cookies && cookies.length > 0) {
+    cookies.forEach(cookie => {
+      response.cookie(cookie.name, cookie.value, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        ...cookie.options,
+      })
+    })
+  }
+
   return response.status(status).send(message)
 }
