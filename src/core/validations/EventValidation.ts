1e60eff4e45055294e0ac99f0a8aadc99345feb7
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-08-10 15:35:24 -0400

    refactor: add missing fields to CreateEventSchema and UpdateEventSchema

--- src/core/validations/EventValidation.ts
+++ src/core/validations/EventValidation.ts
@@ -1,4 +1,5 @@
-import { object, string, coerce } from 'zod'
+import { object, string, coerce, enum as enum_, nativeEnum } from 'zod'
+import { EventStatus } from '../../adapters/typeorm/schema/Event'
 
 export const CreateEventSchema = object({
   name: string().min(1).max(80),
@@ -9,6 +10,9 @@ export const CreateEventSchema = object({
   location: string().min(1),
   banner_url: string().url(),
   client_id: string().uuid(),
+  registration_method: enum_(['email', 'rut', 'both']).default('email'),
+  allow_public_evaluation: coerce.boolean().default(false),
+  status: nativeEnum(EventStatus).default(EventStatus.Pending),
 })
 
 export const UpdateEventSchema = object({
@@ -19,4 +23,7 @@ export const UpdateEventSchema = object({
   date_end: coerce.date().optional(),
   location: string().min(1).optional(),
   banner_url: string().url().optional(),
+  registration_method: enum_(['email', 'rut', 'both']).optional(),
+  allow_public_evaluation: coerce.boolean().optional(),
+  status: nativeEnum(EventStatus).optional(),
 })
